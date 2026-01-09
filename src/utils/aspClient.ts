f2027aa162fc53e5a67a946cdbaa4456c40422bb
Patrick Aljord <patcito@gmail.com>
2026-01-09 16:47:13 +0100

    fix: update asp calls

--- src/utils/aspClient.ts
+++ src/utils/aspClient.ts
@@ -5,9 +5,18 @@ const {
   constants: { ITEMS_PER_PAGE },
 } = getConfig();
 
-const SCOPE_HEADER = 'X-Pool-Scope';
+const fetchWithHeaders = async <T>(url: string, headers?: Record<string, string>): Promise<T> => {
+  const response = await fetch(url, {
+    headers: {
+      ...headers,
+    },
+  });
 
-const fetchWithHeaders = async <T>(url: string, init?: RequestInit): Promise<T> => {
+  if (!response.ok) throw new Error(`Request failed: ${response.statusText}`);
+  return response.json();
+};
+
+const fetchWithInit = async <T>(url: string, init?: RequestInit): Promise<T> => {
   const response = await fetch(url, init);
 
   if (!response.ok) throw new Error(`Request failed: ${response.statusText}`);
@@ -17,28 +26,22 @@ const fetchWithHeaders = async <T>(url: string, init?: RequestInit): Promise<T> 
 const aspClient = {
   fetchPoolInfo: (aspUrl: string, chainId: string, scope: string) =>
     fetchWithHeaders<PoolResponse>(`${aspUrl}/${chainId}/public/pool-info`, {
-      headers: {
-        [SCOPE_HEADER]: scope,
-      },
+      'X-Pool-Scope': scope,
     }),
 
   fetchAllEvents: (aspUrl: string, chainId: string, scope: string, page = 1, perPage = ITEMS_PER_PAGE) =>
     fetchWithHeaders<AllEventsResponse>(`${aspUrl}/${chainId}/public/events?page=${page}&perPage=${perPage}`, {
-      headers: {
-        [SCOPE_HEADER]: scope,
-      },
+      'X-Pool-Scope': scope,
     }),
 
   fetchDepositsByLabel: (aspUrl: string, chainId: string, scope: string, labels: string[]) =>
     fetchWithHeaders<DepositsByLabelResponse>(`${aspUrl}/${chainId}/public/deposits-by-label`, {
-      headers: {
-        [SCOPE_HEADER]: scope,
-        'X-Labels': labels.join(','),
-      },
+      'X-Pool-Scope': scope,
+      'X-Labels': labels.join(','),
     }),
 
   fetchDepositsByLabelAndScope: (aspUrl: string, chainId: string, depositsGroupedByScope: Record<string, string[]>) =>
-    fetchWithHeaders<Record<string, DepositsByLabelResponse>>(`${aspUrl}/${chainId}/public/deposits-by-label`, {
+    fetchWithInit<Record<string, DepositsByLabelResponse>>(`${aspUrl}/${chainId}/public/deposits-by-label`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -48,16 +51,12 @@ const aspClient = {
 
   fetchMtRoots: (aspUrl: string, chainId: string, scope: string) =>
     fetchWithHeaders<MtRootResponse>(`${aspUrl}/${chainId}/public/mt-roots`, {
-      headers: {
-        [SCOPE_HEADER]: scope,
-      },
+      'X-Pool-Scope': scope,
     }),
 
   fetchMtLeaves: (aspUrl: string, chainId: string, scope: string) =>
     fetchWithHeaders<MtLeavesResponse>(`${aspUrl}/${chainId}/public/mt-leaves`, {
-      headers: {
-        [SCOPE_HEADER]: scope,
-      },
+      'X-Pool-Scope': scope,
     }),
 };
 
